3a1b3f687e833c8f48753d98ff5cbf42fe825069
Hosseyn Alavi <s.m.hosseyn.alavi@gmail.com>
2019-10-20 11:22:55 +0330

    remove depricated pakages

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -1,4 +1,4 @@
-import '@storybook/addon-knobs/register';
+//import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-console';
 import '@storybook/addon-storysource/register';
