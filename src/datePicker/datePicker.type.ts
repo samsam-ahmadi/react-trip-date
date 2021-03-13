481eadbda457d2965e0eb16468657aa1d89faa0a
Samsam Ahmadi <sam.ahmadi34@gmail.com>
2021-03-13 14:02:52 +0330

    refactor(date-picker): remove extra types

--- src/datePicker/datePicker.type.ts
+++ src/datePicker/datePicker.type.ts
@@ -1,5 +1,3 @@
-import { Dayjs } from "dayjs";
-
 export type DatePickerType = {
   jalali?: boolean;
 };
@@ -9,15 +7,12 @@ export type DatePickerOnChange = (days: string[]) => void;
 export type ResponsiveHandler = (howManyDoYouWantToShow: any) => boolean;
 
 export interface DatePickerProps {
-  dayComponent?: React.FC<{ day: Dayjs }>;
   numberOfSelectableDays?: number;
   disabledBeforeToday?: boolean;
   disabledDays?: string[];
-  disabled?: boolean;
   jalali?: boolean;
   startOfWeek?: number;
   selectedDays?: string[];
   numberOfMonths?: number;
   onChange: DatePickerOnChange;
-  responsive?: ResponsiveHandler;
 }
