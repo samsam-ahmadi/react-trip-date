a35da01d66baec93806ec99b4f1f6f42e763f525
Samsam Ahmadi <sam.ahmadi34@gmail.com>
2021-03-31 01:15:03 +0430

    feat: export calendar component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { RangePicker } from "./rangePicker";
 export { DatePicker } from "./datePicker";
+export { Calendar } from "./calendar";
+export type { CalendarProps } from "./calendar/calendar.type";
