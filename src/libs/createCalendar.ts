944e050fb9eff6f86ec70b6fe74dc93893259486
Colin Guthrie <colin@booksterhq.com>
2021-06-23 16:49:18 +0100

    fix: correct rendering when startOfWeek has non-zero values

--- src/libs/createCalendar.ts
+++ src/libs/createCalendar.ts
@@ -8,17 +8,16 @@ const createCalendar = ({
   const daysInMonth = source.daysInMonth();
   const firstDay = source.startOf("month").day();
 
-  let shift =
-    firstDay + startOfWeek === 7
-      ? 0
-      : firstDay + startOfWeek < 7
-      ? firstDay + startOfWeek
-      : 7 - firstDay + startOfWeek;
+  let sow = startOfWeek < 0 ? 0 : startOfWeek % 7;
+  let shift = 0;
+  if (firstDay > sow) shift = firstDay - sow;
+  else if (sow > firstDay) shift = 7 - (sow - firstDay);
 
   let tmpDay = source.startOf("month").subtract(shift, "day");
   let totalDays = daysInMonth + shift;
   let remaining = totalDays % 7;
-  for (let i = 0; i < totalDays + (remaining > 0 ? 7 - remaining : 0); i++) {
+  if (remaining > 0) totalDays += 7 - remaining;
+  for (let i = 0; i < totalDays; i++) {
     days.push(tmpDay);
     tmpDay = tmpDay.add(1, "day");
   }
